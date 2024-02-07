e8c8920b59296de874769474c06f6a0c60674c37
alucky36572 <alucky36572@gmail.com>
2024-02-07 15:38:08 +0800

    商品群組跟訂單連php

---
+++ productClassDataGet.php
@@ -0,0 +1,26 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+$host = "localhost";
+$dbname = "food";
+$username = "root";
+$password = "";
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $sql = "SELECT product_class_no, product_class_name FROM product_class";
+
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+
+    $productClass = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    header('Content-Type: application/json');
+    echo json_encode($productClass);
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+$conn = null;
+?>
