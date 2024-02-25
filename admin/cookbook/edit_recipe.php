c9e5d11a3832dac5df4a329603593b900e3e277a
covncovy <covncovy@gmail.com>
2024-02-25 20:36:38 +0800

    食譜

--- admin/cookbook/edit_recipe.php
+++ admin/cookbook/edit_recipe.php
@@ -17,13 +17,33 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         $recipe_info = $_POST["recipe_info"];
         $recipe_status = $_POST["recipe_status"];
         
+        // 檢查是否有上傳圖片
+        if (isset($_FILES["recipe_img"]) && $_FILES["recipe_img"]["error"] === 0) {
+            $dir = "../../../image/cookbook/";
+            if (!file_exists($dir)) {
+                mkdir($dir, 0755, true);
+            }
+            
+            // 取得圖片副檔名
+            $fileExt = pathinfo($_FILES["recipe_img"]["name"], PATHINFO_EXTENSION);
+            // 產生新的圖片檔名
+            $fileImg = uniqid() . '.' . $fileExt;
+            $to = realpath($dir) . DIRECTORY_SEPARATOR . $fileImg; // 新檔案名稱的絕對路徑
+            move_uploaded_file($_FILES["recipe_img"]["tmp_name"], $to);
+        } else {
+            // 使用原本的圖片
+            $fileImg = isset($_POST["image"]) ? $_POST["image"] : null;
+
+        }
+        
         $sql = "UPDATE recipe 
                 SET recipe_name = :recipe_name, 
                     recipe_people = :recipe_people,
                     recipe_time = :recipe_time,
                     recipe_ingredient = :recipe_ingredient,
                     recipe_info = :recipe_info, 
-                    recipe_status = :recipe_status
+                    recipe_status = :recipe_status,
+                    recipe_img = :recipe_img
                 WHERE recipe_no = :recipe_no";
         
         $stmt = $pdo->prepare($sql);
@@ -33,6 +53,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         $stmt->bindValue(":recipe_ingredient", $recipe_ingredient);
         $stmt->bindValue(":recipe_info", $recipe_info);
         $stmt->bindValue(":recipe_status", $recipe_status);
+        $stmt->bindValue(":recipe_img", $fileImg);
         $stmt->bindValue(":recipe_no", $recipe_no);
         
         try {
