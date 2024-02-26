4e74c72fb0a05e07997c07e62e5527b4f3542aa2
fafamia <miapyu88@gmail.com>
2024-02-26 12:26:31 +0800

    後臺會員顯示.修改

--- admin/game/deleteGameData.php
+++ admin/game/deleteGameData.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: http://localhost:5173");
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 header('Content-Type: application/json');
--- front/member/front_checkLogin.php
+++ front/member/front_checkLogin.php
@@ -1,8 +1,7 @@
 <?php
-header("Access-Control-Allow-Origin: http://localhost:5174");
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-header("Access-Control-Allow-Credentials:true");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 header("Content-Type:application/json;charset=utf-8");
 
 $data = json_decode(file_get_contents("php://input"), true);
