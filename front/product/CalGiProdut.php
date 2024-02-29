418584e2c225ed1ebccbe9b259530d9c132cd33a
dis90503dis <dis90503dis@gmail.com>
2024-02-29 13:27:51 +0800

    JION TAG

--- front/product/CalGiProdut.php
+++ front/product/CalGiProdut.php
@@ -5,7 +5,9 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 require_once("../../connect_chd104g3.php");
 
 try {
-    $sql = "SELECT * FROM product ORDER BY RAND() LIMIT 3;";
+    $sql = "SELECT * FROM product p 
+        JOIN product_tag pt ON p.product_tag_no = pt.product_tag_no 
+        ORDER BY RAND() LIMIT 3;";
     $stmt = $pdo->prepare($sql);
     $stmt->execute();
     $products = $stmt->fetchAll(PDO::FETCH_ASSOC); 
